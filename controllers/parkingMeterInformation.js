53e90cb8c3f7a9d254a8fd52f10e23c32a3dab3a
jennifer hobbs <jen.g.hobbs@gmail.com>
2020-01-11 22:21:34 -0800

    in the middle of creating a search functionality

--- controllers/parkingMeterInformation.js
+++ controllers/parkingMeterInformation.js
@@ -3,20 +3,23 @@ let tickets = require('../model/parkingTickets');
 let ax = require('axios');
 
 function fetchMeters() {
-    ax.get('https://opendata.vancouver.ca/api/v2/catalog/datasets/parking-meters/exports/json?rows=-1&pretty=false&timezone=UTC')
-    .then(function (response) {
-        console.log(response);
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
+    // ax.get('https://opendata.vancouver.ca/api/v2/catalog/datasets/parking-meters/exports/json?rows=-1&pretty=false&timezone=UTC')
+    // .then(function (response) {
+    //     console.log(response);
+    // })
+    // .catch(function (error) {
+    //     console.log(error);
+    // });
 }
 
 exports.getTable = (req,res) => {
     fetchMeters();
-    let information = meters.check();
-
+    let information = null
     let ticket = tickets.parkingTickets();
+    if(meters.getLocations() == undefined ){
+    information = meters.check();
+
+    
 
     ax.get('https://opendata.vancouver.ca/api/v2/catalog/datasets/parking-tickets-2017-2019/exports/json?rows=2000&pretty=false&timezone=UTC')
         .then(function (response) {
@@ -68,12 +71,31 @@ exports.getTable = (req,res) => {
         .catch(function (error) {
             console.log(error);
         });
-        
-
+        testing();
+    }
+    else{   
+        console.log("information isnt null");
+        console.log(meters.getLocations());
+        information = meters.getLocations();
+        console.log(information);
+    }
     res.render('index', {street: information, ticket: ticket});
     // information.then(([rows, fieldData])=>{
     //     res.render('index', {street:rows});
 
     // });
 
+}
+
+function testing(){
+    let information = ['one', 'two', 'three', 'four'];
+    meters.setLocations(information);
+    
+}
+exports.setRange= (req,res) => {
+    let dateStart = req.body.dateStart;
+    let dateEnd = req.body.dateEnd;
+    let information = meters.getLocations(dateStart, dateEnd);
+    console.log(information[0]);
+    res.redirect(301, '/');
 }
--- model/parkingMeters.js
+++ model/parkingMeters.js
@@ -1,4 +1,5 @@
 ///call json
+var information;
 function check(){
     console.log("checking check function");
     var streetName = ['one', 'two', 'three'];
@@ -7,7 +8,24 @@ function check(){
 function parkingMeters(){
     
 }
+function setLocations(locations){
+    information = locations;
+    console.log(information[0]);
+}
+function getLocations(dateStart, dateEnd){
+    console.log(information);
+    let info = [];
+    let count = 0;
+    if(dateStart != null && dateEnd !== null){
+        for(let x = dateStart; x <= dateEnd; x++){
+            info[count] = information[x];
+        }
+    }
+    return info;
+}
 module.exports = {
+    setLocations: setLocations,
     check : check,
-    parkingMeters : parkingMeters
+    parkingMeters : parkingMeters,
+    getLocations : getLocations
 }
